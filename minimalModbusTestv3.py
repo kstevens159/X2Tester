10cf8ebf87f71e10a3e24156107fc08fd61d3e91
Kevin S <kstevens159@gmail.com>
2016-06-28 16:24:35 -0400

    Retry function created

--- minimalModbusTestv3.py
+++ minimalModbusTestv3.py
@@ -37,24 +37,46 @@ def AddressChangeTest(x2):
     print (datetime.now())
     print ("Testing reading & writing Modbus address")
 
-    try:
-        print("Reading address...")
-        origAdd = x2.read_registers(0x1000,1,functioncode=4)
-        print("The device's original address is",origAdd)
+    print("Reading address...")
+    readResult = mbReadWithRetires(0x1000,add)
+    if(readResult):
+        print("The device's original address is",readResult)
+    else:
+        print("The read was not successful")
 
-        if(origAdd==[1]): #if address is already 1
-            add=[2]
-            x2.write_registers(0x1000,add)#write address 2
-        else:
-            add=[1]
-            x2.write_registers(0x1000,add)
 
-        changeAdd=x2.read_regisers(0x1000,1,functioncode=4)
 
-        print("The address was changed to", changeAdd)
+##    try:
+##        print("Reading address...")
+##        origAdd = x2.read_registers(0x1000,1,functioncode=4)
+##        print("The device's original address is",origAdd)
+##
+##        if(origAdd==[1]): #if address is already 1
+##            add=[2]
+##            x2.write_registers(0x1000,add)#write address 2
+##        else:
+##            add=[1]
+##            x2.write_registers(0x1000,add)
+##
+##        changeAdd=x2.read_regisers(0x1000,1,functioncode=4)
+##
+##        print("The address was changed to", changeAdd)
+##
+##    except Exception as err:
+##        print("An error occured on the address read:", err)
 
-    except Exception as err:
-        print("An error occured on the address read:", err)    
+        
+def mbReadWithRetries(reg,add,retries=3):
+    for i in range (0,retries):
+        try:
+            result=x2.read_registers(reg,add,functioncode=4)
+            break #if it gets past the read without causing an exception exit the loop as the read was successful
+        except:
+            pass #Continue running the code without exiting the program if the read was not successful
+    else: #If it exits normally that means it failed every time
+        return False
+    return result
+            
 
 #This function replaces the standard address range (0-247) check with an extended range (0-255)
 def _checkSlaveaddress(slaveaddress):
