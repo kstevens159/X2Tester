c83451d00e6c6c6d8cd71dc7124773914c913c63
Kevin Stevens <kstevens159@gmail.com>
2016-06-30 16:40:41 +0000

    Update print statements in wifiScan

--- TestCodeScripts/wifiScan.py
+++ TestCodeScripts/wifiScan.py
@@ -1,21 +1,27 @@
 from wifi import Cell
 import time
 
-searchString="Zy"
+searchString="X2 Logger"
+retries=5
+sleepSec = 10
 
-for i in range(0,5):
+for i in range(0,retries):
     ssids=[cell.ssid for cell in Cell.all('wlan0')]
-    print(ssids)
+    print("List of all networks found:",ssids)
     for ssid in ssids:
         if (searchString in ssid):
             done=True
-            print("Successfully found network:",ssid)
+            print("Attempt", i+1, "of", retries, "was successful")
+            print("Found network:",ssid)
             break
         else:
             done=False
     if(done):
         break
     else:
-        print("Failed to find a network with", searchString, "in it on attempt", i+1)
-        print("Waiting and retrying")
-        time.sleep(10)
+        print("Failed to find a network with", searchString, "in it on attempt", i+1, "of", retries)
+        if(i+1<retries):
+            print("Waiting", sleepSec, "seconds and retrying...")
+            time.sleep(sleepSec)
+        else:
+            print("Failed to find network")
